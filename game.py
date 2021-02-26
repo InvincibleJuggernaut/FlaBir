c74cb08d228cbc612cafa5bd74bd1d558433f3cd
Manikandan Nair <mani4pmn@gmail.com>
2021-02-26 09:13:15 +0000

    Add files via upload

--- game.py
+++ game.py
@@ -1,9 +1,27 @@
-import pygame, sys
+import pygame, sys, random
 
 def draw_floor():
     screen.blit(floor_surface, (floor_x_pos, 900))
     screen.blit(floor_surface, (floor_x_pos+576, 900))
 
+def create_pipe():
+    random_pipe_pos = random.choice(pipe_height)
+    bottom_pipe = pipe_surface.get_rect(midtop=(700, random_pipe_pos))
+    top_pipe = pipe_surface.get_rect(midbottom=(700, random_pipe_pos - 300))
+    return bottom_pipe, top_pipe
+
+def move_pipes(pipes):
+    for pipe in pipes:
+        pipe.centerx-=5
+    return pipes
+
+def draw_pipes(pipes):
+    for pipe in pipes:
+        if pipe.bottom>=1024:
+            screen.blit(pipe_surface, pipe)
+        else:
+            flip_pipe = pygame.transform.flip(pipe_surface, False, True)
+            screen.blit(flip_pipe, pipe)
 
 pygame.init()
 
@@ -24,6 +42,13 @@ bird_surface = pygame.image.load('Assets/images/bluebird-midflap.png').convert()
 bird_surface = pygame.transform.scale2x(bird_surface)
 bird_rect = bird_surface.get_rect(center = (100,512))
 
+pipe_surface = pygame.image.load('Assets/images/pipe-green.png')
+pipe_surface = pygame.transform.scale2x(pipe_surface)
+pipe_list = []
+spawnpipe = pygame.USEREVENT
+pygame.time.set_timer(spawnpipe, 1200)
+pipe_height = [400, 600, 800]
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -33,12 +58,20 @@ while True:
             if event.key == pygame.K_SPACE:
                 bird_movement=0
                 bird_movement-=12
+        if event.type == spawnpipe:
+            pipe_list.extend(create_pipe())
+
+
 
     screen.blit(background_surface, (0,0))
 
     bird_movement+=gravity
     bird_rect.centery+=bird_movement
     screen.blit(bird_surface, bird_rect)
+
+    pipe_list = move_pipes(pipe_list)
+    draw_pipes(pipe_list)
+
     floor_x_pos -=1
     draw_floor()
     if(floor_x_pos<=-576):
