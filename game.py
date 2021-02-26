72d8e914e4b708195a0771e0dcca875c4e5866ae
Manikandan Nair <mani4pmn@gmail.com>
2021-02-26 10:02:09 +0000

    Add files via upload

--- game.py
+++ game.py
@@ -32,6 +32,10 @@ def check_collision(pipes):
         return False
     
     return True
+
+def rotate_bird(bird):
+    new_bird = pygame.transform.rotozoom(bird, -bird_movement * 3, 1)
+    return new_bird
         
 
 pygame.init()
@@ -50,7 +54,7 @@ floor_surface = pygame.image.load('Assets/images/base.png').convert()
 floor_surface = pygame.transform.scale2x(floor_surface)
 floor_x_pos = 0
 
-bird_surface = pygame.image.load('Assets/images/bluebird-midflap.png').convert()
+bird_surface = pygame.image.load('Assets/images/bluebird-midflap.png').convert_alpha()
 bird_surface = pygame.transform.scale2x(bird_surface)
 bird_rect = bird_surface.get_rect(center = (100,512))
 
@@ -85,8 +89,9 @@ while True:
 
     if(game_active):
         bird_movement+=gravity
+        rotated_bird = rotate_bird(bird_surface)
         bird_rect.centery+=bird_movement
-        screen.blit(bird_surface, bird_rect)
+        screen.blit(rotated_bird, bird_rect)
         game_active = check_collision(pipe_list)
 
         pipe_list = move_pipes(pipe_list)
